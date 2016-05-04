83ee3eb9e1b6ced0c854dda7c59c8adc2bfe6086
carbotan <mtanner@gmail.com>
2016-05-04 14:54:50 +0000

    Everything works except for LOSE: YES.  Going to create MAIN function to fix this issue.

--- hangmanREDONE.py
+++ hangmanREDONE.py
@@ -1,19 +1,25 @@
 import random
 import re
 import requests
+from hangmanPics import HANGMANPICS
 
 #Global variables: wordList, wrongGuessCounter, usedLetters, secretWord
-apiWord = requests.get("http://randomword.setgetgo.com/get.php?len=7")
 
 wrongGuessCounter = 0
-secretWord = apiWord.text
 usedLetters = []
+activeGame = True 
 
-apiWord = requests.get("http://randomword.setgetgo.com/get.php")
-print(apiWord)
+#Get request to random word API
+def apiWord():
+    word = requests.get("http://randomword.setgetgo.com/get.php?len=7")
+    return word.text.lower()
 
-#Display Board function:
+secretWord = apiWord()
+
+#Display Board function: Will take: secretWord.  Will return: Nothing
 def displayBoard(word):
+    #Will one day display ASCII
+    print(HANGMANPICS[wrongGuessCounter])
     #Displays 'hashed' secretWord
     print("")
     print("SECRET WORD -->     ", end="")
@@ -27,7 +33,7 @@ def displayBoard(word):
     print("Your guessed letters:",end=" ")
     for x in sorted(usedLetters):
         print(x.upper(),end=" ")
-    #Will one day display ASCII
+    
 
 #checkInput function:  Make sure input is single letter.  Append correct input to usedLetters
 def checkInput():
@@ -37,17 +43,18 @@ def checkInput():
         if not re.match("[a-zA-Z]", playerGuess):
             print("Error: Only single letters allowed")
             continue
-        elif len(playerGuess) > 1:
+        elif len(playerGuess) != 1:
             print("Error: Only single letters allowed")
             continue
         elif playerGuess in usedLetters:
-            print("Error: The letter '{}' has already been used.  Please guess again.".format(playerGuess))
+            print("Error: The letter '{}' has already been used.  Please guess again.".format(playerGuess.upper()))
             continue
         else:
             usedLetters.append(playerGuess)
             return playerGuess
 
 #isInWord function: Checks if user input is in the secretWord (after checkInput)
+#Will take: guess.  Will return: nothing  If guess is not in secret word, increase wrongGuessCounter by 1
 def isInWord(guess):
     global wrongGuessCounter
     if guess in secretWord:
@@ -55,7 +62,9 @@ def isInWord(guess):
     else: 
         print("The letter '{}' is not in the Secret Word.\n".format(guess.upper()))
         wrongGuessCounter += 1
-        
+
+#Check if player has won with last input (function)
+#If they win, display win message and call playAgain. If not, continue
 def win():
     foundAllLetters = True
     for letters in secretWord:
@@ -71,6 +80,7 @@ def playAgain():
     global wrongGuessCounter
     global usedLetters
     global secretWord
+    global activeGame
     
     startOver = False
     
@@ -78,36 +88,27 @@ def playAgain():
     if startOver == True:
         wrongGuessCounter = 0
         usedLetters = []
-        secretWord = apiWord.text
+        secretWord = apiWord()
     else: 
         print("Have a nice day!")
         exit()
     
 #main body:  Non-function.  Nest within while loop - calls all of the above functions as necessary
-print("Welcome to Hangman! Try to guess the Secret Word. If you guess wrong five\
+print("Welcome to Hangman! Try to guess the Secret Word. If you guess wrong five \
 times the game is over.")
-    
-while wrongGuessCounter < 6:
-    print(secretWord)
-    #Display Board function.  Will take: secretWord.  Will return: Nothing
-    displayBoard(secretWord)
 
-    #Receives input and checks to make sure it is single letter (function) 
-    #Will return: guess (player input that is a single letter)
+   
+#while activeGame == True:
+while activeGame == True and wrongGuessCounter < 5:
+    print(secretWord)
+    displayBoard(secretWord)
     guess = checkInput()
-    
-    #Check input to see if it is in secretWord (function).
-        #Will take: guess
-        #If guess is not in secret word, increase wrongGuessCounter by 1
-        #Will return: nothing
     isInWord(guess)
-        
-    #Check if player has won with last input (function)
-    #If they win, display win message and call playAgain. If not, continue
     win()
         
 #Check if player has lost by running out of turns (non-function)
 #If they lost, display lose message and call playAgain
 else:
+    print(HANGMANPICS[wrongGuessCounter])
     print("You are out of guesses.  The word was {}.".format(secretWord.upper()))
     playAgain()
